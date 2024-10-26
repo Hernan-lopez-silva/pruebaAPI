4883714ff1aa1d7b4bb1b0c15f3802563cd5d58a
hernan <her.lopezs@duocuc.cl>
2024-10-26 11:41:23 -0300

    Creacin de la app

---
+++ HernanLopez18200579-7/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HernanLopez18200579-7',
+  webDir: 'www'
+};
+
+export default config;
